2a1620fa6d55337e00adb9b8741db001ed9d82b4
Justin Bozonier <darkxanthos@gmail.com>
2013-07-24 22:14:10 -0500

    Completed and tested more complicated bayesian network.

--- simple_demo.py
+++ simple_demo.py
@@ -2,13 +2,16 @@ class JointProbabilityTable:
 	def __init__(self, columns, data):
 		self._columns = columns
 		self._probability_index = len(columns)
-		self._data = data#self._normalize(data)
+		self._data = self._normalize(data)
 	def _normalize(self, data):
 		probability_sum = 0
 		for row in data:
 			probability_sum += row[-1]
 		for row in data:
-			row[-1] = row[-1]/probability_sum
+			if probability_sum != 0:
+				row[-1] = row[-1]/probability_sum
+			else:
+				row[-1] = 0
 		return data
 	def given(self, event_name, event_happened_value):
 		contextual_columns = [entry for entry in self._columns]
@@ -25,7 +28,10 @@ class JointProbabilityTable:
 				new_row[-1] = 0
 				contextual_data.append(new_row)
 		for row in contextual_data:
-			row[-1] = row[-1]/probability_sum
+			if probability_sum != 0:
+				row[-1] = row[-1]/probability_sum
+			else:
+				row[-1] = 0
 		return JointProbabilityTable(contextual_columns, contextual_data)
 	def _add_to_current_beliefs(self, current_beliefs, event_value, probability):
 		if not event_value in current_beliefs:
@@ -64,10 +70,9 @@ class JointProbabilityTable:
 				beliefs[event_value] = 0
 			beliefs[event_value] += row[-1]
 		return beliefs
-	def update_applicable_beliefs(self, joint_probability_table):
+	def update_applicable_beliefs(self, node_name, joint_probability_table):
 		for event_name in joint_probability_table._columns:
 			if event_name in self._columns:
-				print "updating belief in " + event_name
 				event_beliefs = joint_probability_table.probability(event_name)
 				self._data = self.update_belief(event_name, event_beliefs)._data
 	def clone(self):
@@ -89,13 +94,11 @@ class BayesianNode:
 		self._affects_nodes.append(node)
 		node.affected_by(self)
 	def _forward_propagate(self, joint_probability_table):
-		print "Forward propagating beliefs on " + str(self._name)
-		self._joint_probability_table.update_applicable_beliefs(joint_probability_table)
+		self._joint_probability_table.update_applicable_beliefs(self._name, joint_probability_table)
 		for affected_node in self._affects_nodes:
 			affected_node._forward_propagate(self._joint_probability_table)
 	def _backward_propagate(self, joint_probability_table):
-		print "Backward propagating beliefs on " + str(self._name)
-		self._joint_probability_table.update_applicable_beliefs(joint_probability_table)
+		self._joint_probability_table.update_applicable_beliefs(self._name, joint_probability_table)
 		for affected_node in self._affected_by:
 			affected_node._backward_propagate(self._joint_probability_table)
 	def given(self, value):
@@ -111,8 +114,8 @@ class BayesianNode:
 				affected_node._backward_propagate(jpt)
 			for affected_node in self._affected_by:
 				affected_node._forward_propagate(jpt)
-	def probability(self, event_value):
-		return self._joint_probability_table.probability(self._name)[event_value]
+	def probability(self):
+		return self._joint_probability_table.probability(self._name)
 	def __str__(self):
 		return str(self._joint_probability_table)
 
@@ -167,66 +170,156 @@ shown_empty_table = JointProbabilityTable(
 		['blue', 'blue',  'blue',    0.0],
 	])
 
-door_picked_node = BayesianNode('door_picked', door_picked_table)
-prize_behind_node = BayesianNode('prize_behind_door', prize_behind_door_table)
-shown_empty_node = BayesianNode('shown_empty', shown_empty_table)
+switch_table = JointProbabilityTable(
+	columns=['switch_or_stay'],
+	data=[
+		['switch',  0.5],
+		['stay', 0.5],
+	])
 
+door_after_choice_table = JointProbabilityTable(
+	columns=['door_picked', 'shown_empty', 'switch_or_stay', 'door_after_choice'],
+	data=[
+		['red', 'red', 'switch', 'red',    0.0],
+		['red', 'red', 'switch', 'green',  0.0],
+		['red', 'red', 'switch', 'blue',   0.0],
+		['red', 'red', 'stay', 'red',      0.0],
+		['red', 'red', 'stay', 'green',    0.0],
+		['red', 'red', 'stay', 'blue',     0.0],
+
+		['red', 'blue', 'switch', 'red',   0.0],
+		['red', 'blue', 'switch', 'blue',  0.0],
+		['red', 'blue', 'switch', 'green', 1.0],
+		['red', 'blue', 'stay', 'red',     1.0],
+		['red', 'blue', 'stay', 'blue',    0.0],
+		['red', 'blue', 'stay', 'green',   0.0],
+
+		['red', 'green', 'switch', 'red',   0.0],
+		['red', 'green', 'switch', 'blue',  1.0],
+		['red', 'green', 'switch', 'green', 0.0],
+		['red', 'green', 'stay', 'red',     1.0],
+		['red', 'green', 'stay', 'blue',    0.0],
+		['red', 'green', 'stay', 'green',   0.0],
+
+		#~~~~~~~~
+
+		['blue', 'red', 'switch', 'red',    0.0],
+		['blue', 'red', 'switch', 'green',  1.0],
+		['blue', 'red', 'switch', 'blue',   0.0],
+		['blue', 'red', 'stay', 'red',      0.0],
+		['blue', 'red', 'stay', 'green',    0.0],
+		['blue', 'red', 'stay', 'blue',     1.0],
+
+		['blue', 'blue', 'switch', 'red',   0.0],
+		['blue', 'blue', 'switch', 'blue',  0.0],
+		['blue', 'blue', 'switch', 'green', 0.0],
+		['blue', 'blue', 'stay', 'red',     0.0],
+		['blue', 'blue', 'stay', 'blue',    0.0],
+		['blue', 'blue', 'stay', 'green',   0.0],
+
+		['blue', 'green', 'switch', 'red',   1.0],
+		['blue', 'green', 'switch', 'blue',  0.0],
+		['blue', 'green', 'switch', 'green', 0.0],
+		['blue', 'green', 'stay', 'red',     0.0],
+		['blue', 'green', 'stay', 'blue',    0.0],
+		['blue', 'green', 'stay', 'green',   1.0],
+
+		#~~~~~~~~
+
+		['green', 'red', 'switch', 'red',    0.0],
+		['green', 'red', 'switch', 'green',  0.0],
+		['green', 'red', 'switch', 'blue',   1.0],
+		['green', 'red', 'stay', 'red',      0.0],
+		['green', 'red', 'stay', 'green',    1.0],
+		['green', 'red', 'stay', 'blue',     0.0],
+
+		['green', 'blue', 'switch', 'red',   1.0],
+		['green', 'blue', 'switch', 'blue',  0.0],
+		['green', 'blue', 'switch', 'green', 0.0],
+		['green', 'blue', 'stay', 'red',     0.0],
+		['green', 'blue', 'stay', 'blue',    1.0],
+		['green', 'blue', 'stay', 'green',   0.0],
+
+		['green', 'green', 'switch', 'red',   0.0],
+		['green', 'green', 'switch', 'blue',  0.0],
+		['green', 'green', 'switch', 'green', 0.0],
+		['green', 'green', 'stay', 'red',     0.0],
+		['green', 'green', 'stay', 'blue',    0.0],
+		['green', 'green', 'stay', 'green',   0.0],
+		
+	])
+
+win_prize_table = JointProbabilityTable(
+	columns=['prize_behind', 'door_after_choice', 'win_prize'],
+	data = [
+		['red', 'red',    True,  1.0],
+		['red', 'red',    False, 0.0],
+		['red', 'green',  True,  0.0],
+		['red', 'green',  False, 1.0],
+		['red', 'blue',   True,  0.0],
+		['red', 'blue',   False, 1.0],
+
+		['green', 'red',  True,  0.0],
+		['green', 'red',  False, 1.0],
+		['green', 'green',True,  1.0],
+		['green', 'green',False, 0.0],
+		['green', 'blue', True,  0.0],
+		['green', 'blue', False, 1.0],
+
+		['blue', 'red',   True,  0.0],
+		['blue', 'red',   False, 1.0],
+		['blue', 'green', True,  0.0],
+		['blue', 'green', False, 1.0],
+		['blue', 'blue',  True,  1.0],
+		['blue', 'blue',  False, 0.0],
+	])
+
+door_picked_node = BayesianNode('door_picked', door_picked_table)
+prize_behind_node = BayesianNode('prize_behind', prize_behind_door_table)
+shown_empty_node = BayesianNode('shown_empty', shown_empty_table)
+win_prize_node = BayesianNode('win_prize', win_prize_table)
+door_after_choice_node = BayesianNode('door_after_choice', door_after_choice_table)
+switch_node = BayesianNode('switch_or_stay', switch_table)
+
+print "Win prize original: " + str(win_prize_table.probability('win_prize'))
+
+# Original
 door_picked_node.affects(shown_empty_node)
 prize_behind_node.affects(shown_empty_node)
 
+# New
+shown_empty_node.affects(door_after_choice_node)
+door_picked_node.affects(door_after_choice_node)
+door_after_choice_node.affects(win_prize_node)
+
+prize_behind_node.affects(win_prize_node)
+switch_node.affects(door_after_choice_node)
+
+def print_all_nodes():
+	print ""
+	print "Door picked: " + str(door_picked_node.probability())
+	print "Prize behind door: " + str(prize_behind_node.probability())
+	print "Door shown empty: " + str(shown_empty_node.probability())
+	print "Win prize: " + str(win_prize_node.probability())
+	print "Updated door choice: " + str(door_after_choice_node.probability())
+	print "Switch or stay: " + str(switch_node.probability())
+	print "~~~~~"
+
+print "Before doing anything..."
+print_all_nodes()
+
 door_picked_node.given('red')
+print "After initially picking the red door..."
+print_all_nodes()
+
 shown_empty_node.given('green')
+print "After being shown the green door..."
+print_all_nodes()
 
-print str(prize_behind_node)
-print str(door_picked_node)
-print str(shown_empty_table)
+switch_node.given('switch')
+print "After switching doors..."
+print_all_nodes()
 
-"""
-print "Testing sprinkler belief updates..."
-p_rain = wet_grass_table.given('wet_grass', True).probability('rain')
-print "Updated rain table: " + str(rain_table.update_belief('rain', p_rain).probability('rain'))
-
-print diner_order_probability.given('order', False)
-adjusted_table = diner_order_probability.update_belief('diner loyalty', {'return': .3, 'new': .7})
-print adjusted_table.update_belief('diner loyalty', {'return': .5, 'new': .5})
-
-print "~~~~~~~~~~~ test smallest table"
-print diner_loyalty_probability.update_belief('diner loyalty', {'return': .7, 'new': .3})
-
-
-
-print "~~~~ Example Scenario ~~~~~~"
-loyalty_probability_given_ordered = diner_order_probability.given('order', True)
-loyalty_beliefs = loyalty_probability_given_ordered.probability('diner loyalty')
-print "Given that an order was placed their loyalty probabilities are:"
-print loyalty_beliefs
-updated_channel_order_probability = channel_order_probability.update_belief('diner loyalty', loyalty_beliefs)
-print "Given that an order was placed their channel probabilities are:"
-print updated_channel_order_probability.probability('channel')
-print "Given that the diner was new their channel probabilities are:"
-print updated_channel_order_probability.given('diner loyalty', 'new')
-
-
-order_node = BayesianNode('order', diner_order_probability)
-channel_node = BayesianNode('channel', channel_order_probability)
-loyalty_node = BayesianNode('diner loyalty', diner_loyalty_probability)
-
-loyalty_node.affects(channel_node)
-loyalty_node.affects(order_node)
-channel_node.affects(order_node)
-
-print 'Given that the diner ordered'
-order_node.given(True)
-print 'The probability they came from a direct channel is: ' + str(channel_node.probability('direct'))
-print 'It should be 0.5483870967741936'
-
-
-#channel_order_probability
-
-#print "P(order) = " + str(diner_order_probability.probability('order'))
-#loyalty_given_channel = channel_order_probability.given('channel', 'seo')
-#new_beliefs = loyalty_given_channel.probability('diner loyalty')
-#print new_beliefs
-#conversion_by_loyalty_given_channel = diner_order_probability.update_belief('diner loyalty', new_beliefs)
-#print conversion_by_loyalty_given_channel
-#print conversion_by_loyalty_given_channel.probability('order')"""
+print "After choosing another color door..."
+door_after_choice_node.given('blue')
+print_all_nodes()
